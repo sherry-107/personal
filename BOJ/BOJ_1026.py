9cd81404422fd6a3a9d458c375e2454bb8b1bf56
sherry-107 <chaseoyoung13@gmail.com>
2026-02-12 16:13:48 +0900

    [BOJ] 보물 - 1026

---
+++ BOJ/BOJ_1026.py
@@ -0,0 +1,15 @@
+t = int(input())
+
+a_arr = list(map(int, input().split()))
+b_arr = list(map(int, input().split()))
+
+a_arr.sort()
+S = 0
+
+for i in range(t):
+    max_v = max(b_arr)
+    S += a_arr[i] * max_v
+    idx = b_arr.index(max_v)
+    b_arr[idx] = -1
+
+print(S)
