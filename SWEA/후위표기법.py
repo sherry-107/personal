f40cf1a2d5f8d47207d1a2fbba5d10346a370fb4
sherry-107 <chaseoyoung13@gmail.com>
2026-02-12 16:13:49 +0900

    [SWEA] 후위표기법 - Stack

---
+++ SWEA/후위표기법.py
@@ -0,0 +1,32 @@
+t = int(input())
+
+for tc in range(1, t+1):
+    arr = list(map(str, input().split()))
+    stack = []
+
+    for i in arr:
+        if i in '+-*/':
+            if len(stack) < 2:
+                result = 'error'
+                break
+            else:
+                num2 = int(stack.pop())
+                num1 = int(stack.pop())
+
+                if i == '+':
+                    calc = num1 + num2
+                elif i == '-':
+                    calc = num1 - num2
+                elif i == '*':
+                    calc = num1 * num2
+                else:
+                    calc = num1 // num2
+                stack.append(str(calc))
+        elif i == '.':
+            if len(stack) == 1:
+                result = stack.pop()
+            else:
+                result = 'error'
+        else:
+            stack.append(i)
+    print(f'#{tc}', result)
