98525b95a87f99b754011203124f554756a8fd87
sherry-107 <chaseoyoung13@gmail.com>
2026-02-12 23:38:46 +0900

    [CODETREE] 3일차 - 사각형의 총 넓이

---
+++ CODETREE/3일차/사각형의 총 넓이.py
@@ -0,0 +1,22 @@
+n = int(input())
+x1, y1, x2, y2 = [], [], [], []
+for _ in range(n):
+    a, b, c, d = map(int, input().split())
+    x1.append(a)
+    y1.append(b)
+    x2.append(c)
+    y2.append(d)
+
+# Please write your code here.
+
+dat = [[0]*250 for i in range(250)]
+cnt = 0
+
+for i in range(n):
+    for x in range(x1[i], x2[i]):
+        for y in range(y1[i], y2[i]):
+            if dat[x][y] == 0:
+                cnt += 1
+            dat[x][y] = 1
+
+print(cnt)
