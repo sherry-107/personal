64675c0208e3878cd70c82d2bbee9b778e6a6962
sherry-107 <chaseoyoung13@gmail.com>
2026-02-16 14:08:52 +0900

    [SWEA] User Problem - 우주 괴물

---
+++ SWEA/D1/우주 괴물.py
@@ -0,0 +1,33 @@
+t = int(input())
+
+for tc in range(1, t+1):
+    n = int(input())
+    arr = [list(map(int, input().split())) for i in range(n)]
+
+    delta1 = [1, -1, 0, 0]
+    delta2 = [0, 0, 1, -1]
+
+    for x in range(n):
+        for y in range(n):
+            if arr[x][y] == 2:
+                
+                for i in range(4):
+                    j = 0
+                    while j < n:
+                        j += 1
+                        x1 = x + delta1[i] * j
+                        y1 = y + delta2[i] * j
+
+                        if 0 <= x1 < n and 0 <= y1 < n:
+                            if arr[x1][y1] == 1:
+                                break
+                            else:
+                                arr[x1][y1] = 9
+
+    cnt = 0
+    for x in range(n):
+        for y in range(n):
+            if arr[x][y] == 0:
+                cnt += 1
+
+    print(f'#{tc} {cnt}')
