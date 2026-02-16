772a2c02f92fb2407024a2ce882b94155868a780
sherry-107 <chaseoyoung13@gmail.com>
2026-02-16 12:07:59 +0900

    [SWEA] IM기출 - 답안지 채점

--- SWEA/D3/답안지 채점.py
+++ SWEA/D3/답안지 채점.py
@@ -4,25 +4,23 @@ for tc in range(1, t+1):
     n, m = map(int, input().split())
 
     ans_arr = list(map(int, input().split()))
-    stu_arr = [list(map(int, input().split())) for i in range(n)]
+    pro_arr = [list(map(int, input().split())) for i in range(n)]
+
     score_arr = []
 
-    for student in stu_arr:
-        if student[0] == ans_arr[0]:
-            score_sum = 1
-        else:
-            score_sum = 0
-
-        for i in range(1, m):
-            score = 0
-            if student[i] == ans_arr[i]:
+    for pro in pro_arr:
+        v = 0
+        for i in range(m):
+            v1 = 0
+            if pro[i] == ans_arr[i]:
                 for j in range(0, i):
-                    if student[j] == ans_arr[j]:
-                        score += 1
+                    if pro[j] == ans_arr[j]:
+                        v1 += 1
                     else:
-                        score = 0
-                score_sum += score + 1
-        score_arr.append(score_sum)
-    
+                        v1 = 0
+
+                v += v1+1
+
+        score_arr.append(v)
+
     print(f'#{tc}', max(score_arr) - min(score_arr))
-    
