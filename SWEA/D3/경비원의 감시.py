4a59df86216ec0f5a100b3affee40fb5bf702b5b
sherry-107 <chaseoyoung13@gmail.com>
2026-02-16 11:34:52 +0900

    [SWEA] IM기출 - 경비원의 감시

--- SWEA/D3/경비원의 감시.py
+++ SWEA/D3/경비원의 감시.py
@@ -4,32 +4,32 @@ for tc in range(1, t+1):
     n = int(input())
 
     arr = [list(map(int, input().split())) for i in range(n)]
-
     delta1 = [1, -1, 0, 0]
     delta2 = [0, 0, 1, -1]
 
+    v = 0
+
     for x in range(n):
         for y in range(n):
+
             if arr[x][y] == 2:
+                for i in range(4):
+                    j = 0
+                    while j <= n-1:
+                        j += 1
                 
-                i = 0
-                while i < 4:
-                    for j in range(1, n):
                         x1 = x + delta1[i] * j
                         y1 = y + delta2[i] * j
 
-                        if 0<=x1<n and 0<=y1<n:
+                        if 0 <= x1 < n and 0 <= y1 < n:
                             if arr[x1][y1] == 1:
                                 break
-                            if arr[x1][y1] == 0:
-                                arr[x1][y1] = 5
-                    i += 1
+                            else:
+                                arr[x1][y1] = 9
 
-    sum_v = 0
-    for i in range(n):
-        for j in range(n):
-            if arr[i][j] == 0:
-                sum_v += 1
+    for x in range(n):
+        for y in range(n):
+            if arr[x][y] == 0:
+                v += 1
 
-    print(f'#{tc}', sum_v)
-    
+    print(f'#{tc} {v}')
