c2f358955e60f47d94c6d056dfdf1b32bcd6de1a
sherry-107 <chaseoyoung13@gmail.com>
2026-02-17 21:46:50 +0900

    [SWEA] IM기출 - LED 조명 제어

--- SWEA/D3/LED 조명 제어.py
+++ SWEA/D3/LED 조명 제어.py
@@ -2,19 +2,17 @@ t = int(input())
 
 for tc in range(1, t+1):
     n = int(input())
-    find_arr = list(map(int, input().split()))
-    find_arr.insert(0, 0)
-    prob_arr = [0]*(n+1)
+    ans_arr = list(map(int, input().split()))
+    pro_arr = [0]*n
+
     cnt = 0
-
-    for i in range(0, n+1):
-        if find_arr[i] != prob_arr[i]:
+    for i in range(n):
+        if ans_arr[i] != pro_arr[i]:
             cnt += 1
-
-            for j in range(i, n+1, i):
-                if prob_arr[j] == 0:
-                    prob_arr[j] = 1
+            for j in range(i, n, i+1):
+                if pro_arr[j] == 0:
+                    pro_arr[j] = 1
                 else:
-                    prob_arr[j] = 0
+                    pro_arr[j] = 0
 
     print(f'#{tc} {cnt}')
