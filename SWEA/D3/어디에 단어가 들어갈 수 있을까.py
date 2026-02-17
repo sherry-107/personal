5e18a45da096aaca874ec26686fa17b952481de1
sherry-107 <chaseoyoung13@gmail.com>
2026-02-17 22:08:04 +0900

    [SWEA] IM기출 - 어디에 단어가 들어갈 수 있을까

--- SWEA/D3/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D3/어디에 단어가 들어갈 수 있을까.py
@@ -2,50 +2,46 @@ t = int(input())
 
 for tc in range(1, t+1):
     n, k = map(int, input().split())
+
     arr = [list(map(int, input().split())) for i in range(n)]
     cnt = 0
 
+    for x in range(0, n-k+1):
+        for y in range(0, n):
+            if arr[x][y] == 1:
+                idx = True
+                for i in range(1, k):
+                    x1 = x + i
+                    if arr[x1][y] != 1:
+                        idx = False
 
-    for x in range(n):
+                if idx:
+                    if 0 <= x-1 < n:
+                        if arr[x - 1][y] == 1:
+                            idx = False
+                    if 0 <= x + k < n:
+                        if arr[x + k][y] == 1:
+                            idx = False
+                if idx:
+                    cnt += 1
+                    
+    for x in range(0, n):
         for y in range(0, n-k+1):
-            idx = 1
-            for i in range(0, k):
-                if arr[x][y+i] != 1:
-                    idx = 0
+            if arr[x][y] == 1:
+                idx = True
 
-            if idx == 1:
-                if y + k <= n - 1:
-                    if arr[x][y+k] == 1:
-                        idx = 0
-                if y - 1 >= 0:
-                    if arr[x][y-1] == 1:
-                        idx = 0
-            if idx == 1:
-                cnt += 1
-
-    arr2 = []
-
-    for i in range(n):
-        a = []
-        for j in range(n):
-            a.append(arr[j][i])
-        arr2.append(a)
-
-    for x in range(n):
-        for y in range(0, n - k + 1):
-            idx = 1
-            for i in range(0, k):
-                if arr2[x][y + i] != 1:
-                    idx = 0
-
-            if idx == 1:
-                if y + k <= n - 1:
-                    if arr2[x][y + k] == 1:
-                        idx = 0
-                if y - 1 >= 0:
-                    if arr2[x][y - 1] == 1:
-                        idx = 0
-            if idx == 1:
-                cnt += 1
+                for i in range(1, k):
+                    y1 = y + i
+                    if arr[x][y1] != 1:
+                        idx = False
+                if idx:
+                    if 0 <= y - 1 < n:
+                        if arr[x][y-1] == 1:
+                            idx = False
+                    if 0 <= y + k < n:
+                        if arr[x][y + k] == 1:
+                            idx = False
+                if idx:
+                    cnt += 1
 
     print(f'#{tc} {cnt}')
