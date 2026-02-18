cc633252794f8ad8551e1b5649f4f9ebbe9adfd5
sherry-107 <chaseoyoung13@gmail.com>
2026-02-18 20:06:04 +0900

    [SWEA] IM기출(1반) - 공 굴리기 게임

--- SWEA/D1/공 굴리기 게임.py
+++ SWEA/D1/공 굴리기 게임.py
@@ -4,20 +4,21 @@ for tc in range(1, t+1):
     n = int(input())
 
     arr = [list(map(int, input().split())) for i in range(n)]
+
     delta1 = [-1, 1, 0, 0]
     delta2 = [0, 0, -1, 1]
-
-    max_cnt = 0
+    max_v = 0
 
     for xx in range(n):
         for yy in range(n):
+            x = xx
+            y = yy
+            cnt = 0
             move = True
-            cnt = 1
-            x, y = xx, yy
+            min_v = arr[x][y]
 
             while move == True:
-                min_v = arr[x][y]
-                
+
                 for i in range(4):
                     x1 = x + delta1[i]
                     y1 = y + delta2[i]
@@ -28,17 +29,15 @@ for tc in range(1, t+1):
                 if min_v == arr[x][y]:
                     move = False
                 else:
-                    cnt += 1
                     for i in range(4):
                         x1 = x + delta1[i]
                         y1 = y + delta2[i]
 
-                        if 0 <= x1 < n and 0 <= y1 < n and arr[x1][y1] == min_v:
-                            x, y = x1, y1
-                            break
-                                
+                        if 0 <= x1 < n and 0 <= y1 < n:
+                            if min_v == arr[x1][y1]:
+                                x, y = x1, y1
+                                break
+                cnt += 1
+            max_v = max(max_v, cnt)
 
-                       
-            max_cnt = max(max_cnt, cnt)
-
-    print(f'#{tc} {max_cnt}')
+    print(f'#{tc} {max_v}')
