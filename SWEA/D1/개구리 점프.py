0ab212cd90a1b4971494d05fa93a073ca65fb4e3
sherry-107 <chaseoyoung13@gmail.com>
2026-02-18 19:47:43 +0900

    [SWEA] IM기출(1반) - 개구리 점프

--- SWEA/D1/개구리 점프.py
+++ SWEA/D1/개구리 점프.py
@@ -2,26 +2,28 @@ t = int(input())
 
 for tc in range(1, t+1):
     n, k = map(int, input().split())
+
     arr = list(map(int, input().split()))
 
+    
     idx = 0
-
     while True:
         jump = False
-    
+
         for i in range(k, 0, -1):
-            if idx + i < len(arr) and arr[idx + i] == 1:
+            if idx + i < len(arr) and arr[idx+i] == 1:
                 idx = idx + i
                 jump = True
                 break
-
         if not jump:
             break
-
+     
     if idx + k >= len(arr):
-        idx = len(arr) - 1
+        idx = len(arr)-1
     else:
         idx += k
+
     print(f'#{tc} {idx+1}')
 
-            
+
+        
