3b77c3aa090777c603239321fe59efabde24dc6c
sherry-107 <chaseoyoung13@gmail.com>
2026-02-18 19:24:02 +0900

    [SWEA] IM기출 - 두 개의 탑

--- SWEA/D3/두 개의 탑.py
+++ SWEA/D3/두 개의 탑.py
@@ -6,13 +6,29 @@ for tc in range(1, t+1):
     arr = list(map(int, input().split()))
     arr.sort()
 
-    cost = 0
+    top1 = []
+    top2 = []
 
-    for i in range(1, max(m1, m2)+1):
-        if i <= m1:
-            cost += i * arr.pop()
+    top1_v = 0
+    top2_v = 0
 
-        if i <= m2:
-            cost += i * arr.pop()
+    for i in range(1, min(m1, m2) + 1):
+        a = arr.pop()
+        top1.append(a)
+        b = arr.pop()
+        top2.append(b)
+        
+    if m1 > m2:
+        top1 += arr
+    else:
+        top2 += arr
 
-    print(f'#{tc} {cost}')
+    top1.sort(reverse=True)
+    top2.sort(reverse=True)
+
+    for i in range(1, len(top1)+1):
+        top1_v += i * top1[i-1]
+    for i in range(1, len(top2) + 1):
+        top2_v += i * top2[i-1]
+
+    print(f'#{tc} {top1_v + top2_v}')
