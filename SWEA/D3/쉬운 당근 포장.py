9d7f014009dc315a74d318166ab592414b615ddd
sherry-107 <chaseoyoung13@gmail.com>
2026-02-18 19:54:56 +0900

    [SWEA] User Problem - 쉬운 당근 포장

--- SWEA/D3/쉬운 당근 포장.py
+++ SWEA/D3/쉬운 당근 포장.py
@@ -3,37 +3,35 @@ t = int(input())
 for tc in range(1, t+1):
     n = int(input())
     arr = list(map(int, input().split()))
-    
-    carrot = {}
 
+    dict = {}
     min_v = 10000000
 
     for i in arr:
-        carrot[i] = 0
+        dict[i] = 0
 
-    if len(carrot.keys()) <= 2:
+    if len(dict.keys()) <= 2:
         min_v = -1
 
     else:
         for i in arr:
-            carrot[i] += 1
+            dict[i] += 1
 
-        sorted_carrot = sorted(carrot.keys())
+        carrot = list(dict.keys())
 
-        for i in range(len(sorted_carrot) - 2):
-            for j in range(i+1, len(sorted_carrot) - 1):
+        for i in range(len(carrot)-2):
+            for j in range(i+1, len(carrot)-1):
 
-                s_cnt = 0
-                m_cnt = 0
-                l_cnt = 0
+                s = 0
+                m = 0
+                l = 0
 
                 for k in range(0, i+1):
-                    s_cnt += carrot[sorted_carrot[k]]
+                    s += dict[carrot[k]]
                 for k in range(i+1, j+1):
-                    m_cnt += carrot[sorted_carrot[k]]
+                    m += dict[carrot[k]]
 
-                l_cnt = len(arr) - s_cnt - m_cnt
+                l = n - s - m
 
-                min_v = min(min_v, max(s_cnt, m_cnt, l_cnt) - min(s_cnt, m_cnt, l_cnt))
-
+                min_v = min(min_v, max(s, m, l) - min(s, m, l))
     print(f'#{tc} {min_v}')
