d125d06c0c0ad976076ea0f091722ea3b61c9b67
sherry-107 <chaseoyoung13@gmail.com>
2026-03-15 20:54:21 +0900

    [SWEA] Adv기출 - 양봉업자 코코

--- SWEA/D5/양봉업자 코코.py
+++ SWEA/D5/양봉업자 코코.py
@@ -5,42 +5,43 @@ for tc in range(1, t+1):
 
     arr = [list(map(int, input().split())) for i in range(n)]
 
+    odd_delta1 = [1, -1, 0, 0, 1, 1]
+    odd_delta2 = [0, 0, 1, -1, 1, -1]
     even_delta1 = [1, -1, 0, 0, -1, -1]
     even_delta2 = [0, 0, 1, -1, 1, -1]
 
-    odd_delta1 = [1, -1, 0, 0, 1, 1]
-    odd_delta2 = [0, 0, 1, -1, 1, -1]
+    visited = [[False] * m for i in range(n)]
 
-    def honey(x, y, level, cost, visited):
+    max_v = 0
+
+    def honey(level, v, x, y):
+        global max_v
         if level == 3:
-            return cost
-        
-        res = cost
+            max_v = max(v, max_v)
+            return
+
+        result = v
         for i in range(6):
-            if y%2 == 0:
+            if y % 2 == 0:
                 x1 = x + even_delta1[i]
                 y1 = y + even_delta2[i]
+
             else:
                 x1 = x + odd_delta1[i]
                 y1 = y + odd_delta2[i]
 
-            if 0 <= x1 < n and 0 <= y1 < m:
-                if not visited[x1][y1]:
-                    visited[x1][y1] = True
-                    res = max(res, honey(x1, y1, level+1, cost + arr[x1][y1], visited))
-                    
-                    if level == 1:
-                        res = max(res, honey(x, y, level+1, cost + arr[x1][y1], visited))
-                    
-                    visited[x1][y1] = False
-        return res
-    
-    max_v = 0
+            if 0 <= x1 < n and 0 <= y1 < m and not visited[x1][y1]:
+                visited[x1][y1] = True
+                honey(level+1, v+arr[x1][y1], x1, y1)
+
+                if level == 1:
+                    honey(level+1, v+arr[x1][y1], x, y)
+                visited[x1][y1] = False
 
     for x in range(n):
         for y in range(m):
-            visited = [[False] * m for i in range(n)]
             visited[x][y] = True
-            max_v = max(max_v, honey(x, y, 0, arr[x][y], visited))
-    
+            honey(0, arr[x][y], x, y)
+            visited[x][y] = False
+
     print(f'#{tc} {max_v}')
